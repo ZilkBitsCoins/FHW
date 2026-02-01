e4efc342785f200fa3cee35051171a42efa82b6f
ZilkBitsCoins <pm23004@ti.ues.edu.sv>
2026-01-31 20:20:16 -0600

    Practica de numeros aleatorios utilizando random.

---
+++ NumerosAleatorios.py
@@ -0,0 +1,18 @@
+import random
+
+numeros_a_generar = int(input("¿Cuántos números aleatorios deseas generar? "))
+rango_minimo = int(input("Ingresa el valor mínimo del rango: "))
+rango_maximo = int(input("Ingresa el valor máximo del rango: "))
+
+def generar_numeros_aleatorios(cantidad, minimo, maximo):
+    numeros = []
+    for _ in range(cantidad):
+        numero = random.randint(minimo, maximo)
+        numeros.append(numero)
+    return numeros
+
+if __name__ == "__main__":
+    numeros_generados = generar_numeros_aleatorios(numeros_a_generar, rango_minimo, rango_maximo)
+    print("Números aleatorios generados:")
+    for numero in numeros_generados:
+        print(numero)
